b30817bd17cf9b141b4dce245b601f7b890af226
amd989 <alejandro@shift-me.com>
2014-11-18 18:56:42 -0600

    Delete Schema file

--- schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists switch;
-create table switch (
-  switch_id integer primary key autoincrement,
-  output_id integer not null,
-  title text not null,
-  description text not null,
-  state integer not null
-);
